85a437f226e8d2e6f90ce4353702927945496d13
Nicole Wei <weiyichen2004@gmail.com>
2025-10-22 08:28:55 -0500

    workable sidebar

--- recipe/complete/script.js
+++ recipe/complete/script.js
@@ -165,3 +165,18 @@ stepSelector.addEventListener("change", () => {
 
     stepContentText.innerHTML = stepContent[stepNum];
 });
+
+const toggleSidebar = document.getElementById("hamburger-menu");
+const sidebar = document.getElementById("sidebar-mob");
+const sidebarNavs = document.querySelectorAll(".btn-sidebar");
+
+toggleSidebar.addEventListener("click", ()=> {
+    sidebar.showModal();
+    
+})
+
+for (const btn of sidebarNavs) {
+  btn.addEventListener("click", () => {
+    sidebar.close();
+  });
+}
