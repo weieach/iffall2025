55103ce0f89a1ad49198c352afce75f53cd01e6f
Nicole Wei <weiyichen2004@gmail.com>
2025-11-28 10:19:54 -0600

    dark mode first steps

--- microsite/final/js/gsap.js
+++ microsite/final/js/gsap.js
@@ -81,35 +81,6 @@ gsap.to(".ph-arrow-fat-left", {
   ease: "power2.out",
 });
 
-// let zTimeline = gsap.timeline();
-// zTimeline.to()
-
-// svg morph
-
-let btnNight = document.querySelector(".btn-night");
-
-btnNight.addEventListener("mouseenter", () => {
-  gsap.to("#moon-icon", {
-    duration: 1,
-    morphSVG: "#moon-icon-detailed",
-  });
-  gsap.to(btnNight, {
-    backgroundColor: "#0a0a20",
-    duration: 0.2,
-  });
-});
-
-btnNight.addEventListener("mouseleave", () => {
-  gsap.to("#moon-icon", {
-    duration: 1,
-    morphSVG: "#moon-icon",
-  });
-  gsap.to(btnNight, {
-    backgroundColor: "#373331",
-    duration: 0.2,
-  });
-});
-
 let foodTl = gsap.timeline({
   repeat: -1,
   repeatDelay: 1.5,
@@ -132,6 +103,10 @@ foodTl
       ease: "bounce.inOut",
     },"<"
   );
+
+
+// let zTimeline = gsap.timeline();
+// zTimeline.to()
 
 //draw svg
 
@@ -161,3 +136,120 @@ let smoother = ScrollSmoother.create({
   content: "#smooth-content",
   smooth: 2,
 });
+
+
+// button night: svg morph
+
+let btnNight = document.querySelector(".btn-night");
+let btnNightText = document.querySelector(".btn-night p");
+
+btnNight.addEventListener("mouseenter", () => {
+  if(!btnNight.hasAttribute("disabled")){
+    gsap.to("#moon-icon", {
+    duration: 1,
+    morphSVG: "#moon-icon-detailed",
+  });
+  gsap.to(btnNight, {
+    backgroundColor: "#0a0a20",
+    duration: 0.2,
+  });
+  }
+  
+});
+
+btnNight.addEventListener("mouseleave", () => {
+  gsap.to("#moon-icon", {
+    duration: 1,
+    morphSVG: "#moon-icon",
+  });
+  gsap.to(btnNight, {
+    backgroundColor: "#373331",
+    duration: 0.2,
+  });
+  
+});
+
+const state = Flip.getState(".panel-time");
+
+let turnsNightTl = gsap.timeline();
+MorphSVGPlugin.convertToPath("circle, rect, ellipse, line, polygon, polyline");
+btnNight.addEventListener("click", () => {
+  btnNight.setAttribute("disabled", "disabled");
+  btnNightText.textContent = "Say hi to our squirrel!";
+  turnsNightTl.to(".ph-arrow-fat-left", {
+    opacity: 0,
+  })
+  .set("body", {
+    overflow: "auto",
+  })
+  .to(".btn-night, .panel-row p", {
+    opacity: 0.6,
+  });
+
+  Flip.from(state, {
+    duration: 2, 
+    ease: "power1.inOut",
+    order: -1,
+    // absolute: true,
+    nested: true,
+  });
+
+  turnsNightTl.to(".img-sky-texture", {
+    delay: 1,
+    opacity: 0.7,
+    mixBlendMode: "multiply",
+  })
+  .to(".panel-time", {
+    background: "linear-gradient(0deg, #141414 0%, #141414 38%, #45599f 100%)",
+  }, "<")
+  .to(".panel-door", {
+    background: "#141414",
+  }, "<")
+  .to(".h1-col, .scroll-flower, .panel-row, .top-right-panel", {
+    backgroundColor: "#413F3D",
+  }, "<")
+  .to(".h1-col, .panel-row, .logo-flower", {
+    backgroundColor: "#413F3D",
+  }, "<")
+  .to(".h1-scroll, .scroll-flower", {
+    opacity: 0,
+  }, "<")
+  .to(".intro-text", {
+    color: "#EDEEE5",
+  }, "<")
+  .to(".human-quote, .quote-text", {
+    color: "#527259",
+  }, "<")
+  .to(".hearts .ph-heart", {
+    color: "#44579B",
+  }, "<")
+  .to(".img-forest-dark", {
+    opacity: 1,
+  }, "<")
+  .to("#sun-icon path", {
+    duration: 1.2,
+    morphSVG: "#moon-icon-night path",
+    fill: "#92ACD7",
+  })
+  .to(".div-fs-portrait", {
+    scale: 1.05,
+    duration: 0.2,
+  }, "+=1")
+  .to(".img-fs-mask", {
+    delay: 0.5,
+    opacity: 0,
+    duration: 1,
+  })
+  .to(".div-fs-portrait", {
+    scale: 1,
+    duration: 0.2,
+  });
+
+  //tree trunk
+
+  
+  // gsap.set(".img-door", {
+
+  // });
+});
+
