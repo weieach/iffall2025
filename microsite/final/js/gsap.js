53268b4db9aac9387a3892ec03b01d4c5b843aca
Nicole Wei <weiyichen2004@gmail.com>
2025-11-28 19:11:31 -0600

    first quick fact successfully pinned!!!!

--- microsite/final/js/gsap.js
+++ microsite/final/js/gsap.js
@@ -17,7 +17,6 @@ logo.addEventListener("mouseenter", () => {
     scale: 0.7,
     rotation: 60,
     xPercent: -40,
-   
   });
 });
 logo.addEventListener("mouseleave", () => {
@@ -26,7 +25,7 @@ logo.addEventListener("mouseleave", () => {
     scale: 1,
     rotation: 0,
     xPercent: 0,
-     delay: 1,
+    delay: 1,
   });
 });
 
@@ -87,13 +86,11 @@ let foodTl = gsap.timeline({
   yoyo: true,
 });
 foodTl
-  .to(
-    ".img-plate",
-    {
-      duration: 0.2,
-      yPercent: -10,
-      ease: "power4.out",
-    })
+  .to(".img-plate", {
+    duration: 0.2,
+    yPercent: -10,
+    ease: "power4.out",
+  })
   .to(
     ".food-item",
     {
@@ -101,9 +98,9 @@ foodTl
       rotation: "random(-20, 20)",
 
       ease: "bounce.inOut",
-    },"<"
+    },
+    "<"
   );
-
 
 // let zTimeline = gsap.timeline();
 // zTimeline.to()
@@ -115,19 +112,19 @@ let planeTl = gsap.timeline({
   repeatDelay: 1.5,
 });
 planeTl
-  .from(
-    ".plane-path path",
+  .from(".plane-path path", {
+    duration: 2,
+    drawSVG: "0%",
+  })
+  .to(
+    ".img-plane",
     {
-      duration: 2,
-      drawSVG: "0%",
+      opacity: 1,
+      xPercent: 10,
+      duration: 1,
     },
-    
-  )
-  .to(".img-plane", {
-    opacity: 1,
-    xPercent: 10,
-    duration: 1,
-  }, "-=0.3");
+    "-=0.3"
+  );
 
 //smoother
 
@@ -137,7 +134,6 @@ let smoother = ScrollSmoother.create({
   smooth: 2,
 });
 
-
 // button night: svg morph
 
 let btnNight = document.querySelector(".btn-night");
@@ -145,17 +141,16 @@ let btnNightText = document.querySelector(".btn-night p");
 let doorImg = document.querySelector(".img-door");
 
 btnNight.addEventListener("mouseenter", () => {
-  if(!btnNight.hasAttribute("disabled")){
+  if (!btnNight.hasAttribute("disabled")) {
     gsap.to("#moon-icon", {
-    duration: 1,
-    morphSVG: "#moon-icon-detailed",
-  });
-  gsap.to(btnNight, {
-    backgroundColor: "#0a0a20",
-    duration: 0.2,
-  });
+      duration: 1,
+      morphSVG: "#moon-icon-detailed",
+    });
+    gsap.to(btnNight, {
+      backgroundColor: "#0a0a20",
+      duration: 0.2,
+    });
   }
-  
 });
 
 btnNight.addEventListener("mouseleave", () => {
@@ -167,7 +162,6 @@ btnNight.addEventListener("mouseleave", () => {
     backgroundColor: "#373331",
     duration: 0.2,
   });
-  
 });
 
 const state = Flip.getState(".panel-time");
@@ -175,97 +169,224 @@ const state = Flip.getState(".panel-time");
 let turnsNightTl = gsap.timeline();
 MorphSVGPlugin.convertToPath("circle, rect, ellipse, line, polygon, polyline");
 btnNight.addEventListener("click", () => {
+  //stopping old timeline
+  foodTl.restart();
+  foodTl.kill();
+  planeTl.pause();
   btnNight.setAttribute("disabled", "disabled");
   btnNightText.textContent = "Say hi to our squirrel!";
-  turnsNightTl.to(".ph-arrow-fat-left", {
-    opacity: 0,
-  })
-  .set("body", {
-    overflow: "auto",
-  })
-  .to(".btn-night, .panel-row p", {
-    opacity: 0.6,
-  });
+  turnsNightTl
+    .to(".ph-arrow-fat-left", {
+      opacity: 0,
+    })
+    .set("body", {
+      overflow: "auto",
+    })
+    // .set(".panel-distance-food", {
+    //   marginTop: "77vh",
+    // })
+    .to(".btn-night, .panel-row p", {
+      opacity: 0.6,
+    });
 
   Flip.from(state, {
-    duration: 2, 
+    duration: 2,
     ease: "power1.inOut",
     order: -1,
     // absolute: true,
     nested: true,
   });
 
-  turnsNightTl.to(".img-sky-texture", {
-    delay: 1,
-    opacity: 0.7,
-    mixBlendMode: "multiply",
-  })
-  .to(".panel-time", {
-    background: "linear-gradient(0deg, #141414 0%, #141414 38%, #45599f 100%)",
-  }, "<")
-  .to(".panel-door", {
-    background: "#141414",
-  }, "<")
-  .to(".panel-food, .panel-distance", {
-    background: "#141414",
-  }, "<")
-  .to(".h1-col, .scroll-flower, .panel-row, .top-right-panel", {
-    backgroundColor: "#413F3D",
-  }, "<")
-  .to(".h1-col, .panel-row, .logo-flower", {
-    backgroundColor: "#413F3D",
-  }, "<")
-  .to(".h1-scroll, .scroll-flower", {
-    opacity: 0,
-  }, "<")
-  .to(".intro-text", {
-    color: "#EDEEE5",
-  }, "<")
-  .to(".human-quote, .quote-text", {
-    color: "#527259",
-  }, "<")
-  .to(".hearts .ph-heart", {
-    color: "#44579B",
-  }, "<")
-  .to(".img-forest-dark", {
-    opacity: 1,
-  }, "<")
-  .to("#sun-icon path", {
-    duration: 1.2,
-    morphSVG: "#moon-icon-night path",
-    fill: "#92ACD7",
-  })
+  turnsNightTl
+    .to(".img-sky-texture", {
+      delay: 1,
+      opacity: 0.7,
+      mixBlendMode: "multiply",
+    })
+    .set(
+      ".img-ruler",
+      {
+        attr: { src: "assets/ruler-dark.png" },
+      },
+      "<"
+    )
+    .to(
+      ".panel-time",
+      {
+        background:
+          "linear-gradient(0deg, #141414 0%, #141414 38%, #45599f 100%)",
+      },
+      "<"
+    )
+    .to(
+      ".panel-door",
+      {
+        background: "#141414",
+      },
+      "<"
+    )
+    .to(
+      ".panel-food, .panel-distance",
+      {
+        background: "#141414",
+      },
+      "<"
+    )
+    .to(
+      ".scroll-flower, .panel-row, .top-right-panel",
+      {
+        backgroundColor: "#413F3D",
+      },
+      "<"
+    )
+    .to(
+      ".h1-col",
+      {
+        backgroundColor: "#1D1D1D",
+      },
+      "<"
+    )
+    .to(
+      ".h1-col, .panel-row, .logo-flower",
+      {
+        backgroundColor: "#413F3D",
+      },
+      "<"
+    )
+    .to(
+      ".h1-scroll, .scroll-flower",
+      {
+        opacity: 0,
+      },
+      "<"
+    )
+    .to(
+      ".intro-text",
+      {
+        color: "#EDEEE5",
+      },
+      "<"
+    )
+    .to(
+      ".human-quote, .quote-text",
+      {
+        color: "#527259",
+      },
+      "<"
+    )
+    .to(
+      ".hearts .ph-heart",
+      {
+        color: "#44579B",
+      },
+      "<"
+    )
+    .to(
+      ".img-forest-dark",
+      {
+        opacity: 1,
+      },
+      "<"
+    )
+    .to(
+      ".plane-path, .img-plane, .mask",
+      {
+        opacity: 0,
+      },
+      "<"
+    )
+    .to("#sun-icon path", {
+      duration: 1.2,
+      morphSVG: "#moon-icon-night path",
+      fill: "#92ACD7",
+    })
 
-  //reveal portrait
-  .to(".div-fs-portrait", {
-    scale: 1.05,
-    delay: 1,
-    duration: 0.2,
-  })
-  .to(".img-fs-mask", {
-    delay: 0.5,
-    opacity: 0,
-    duration: 1,
-  })
-  .to(".div-fs-portrait", {
-    scale: 1,
-    duration: 0.2,
-  })
-  //open door
-  .to(".img-door-sign, .letter-z", {
-    opacity: 0,
-  })
-  .set(doorImg, {
-    attr: { src: "assets/door-open.png" }
-  })
-  //tree trunk
-  .to(".img-tree-trunk", {
-    opacity: 1,
-  }, "+=1")
+    //reveal portrait
+    .to(".div-fs-portrait", {
+      scale: 1.05,
+      delay: 1,
+      duration: 0.2,
+    })
+    .to(".img-fs-mask", {
+      delay: 0.5,
+      opacity: 0,
+      duration: 1,
+    })
+    .to(".div-fs-portrait", {
+      scale: 1,
+      duration: 0.2,
+    })
+    //open door
+    .to(".img-door-sign, .letter-z", {
+      opacity: 0,
+    })
+    .set(doorImg, {
+      attr: { src: "assets/door-open.png" },
+    })
+    //tree trunk
+    .to(
+      ".img-tree-trunk",
+      {
+        opacity: 1,
+      },
+      "+=1"
+    )
+    //fs enters
+    .set(".img-fs", {
+      scaleX: -1,
+    })
+    .set(".img-fs, .fact-1, .top-right-quickfacts-panel", {
+      display: "block",
+      opacity: 1,
+    });
+  // .to(".img-fs", {
+  //   opacity: 1,
+  // });
 
+  //begins scroll based quick facts
+  ScrollTrigger.create({
+    trigger: ".h1-col",
+    start: "top top",
+    end: "+=200%", // how long you want the grid to stay pinned
+    pin: ".h1-col",
+    pinSpacing: true,
+    anticipatePin: 1,
+  });
+  ScrollTrigger.create({
+    trigger: ".left-col",
+    start: "top top",
+    end: "+=200%", // how long you want the grid to stay pinned
+    pin: ".left-col",
+    pinSpacing: true,
+    anticipatePin: 1,
+  });
+  ScrollTrigger.create({
+    trigger: ".left-col",
+    start: "top top",
+    end: "+=200%", // how long you want the grid to stay pinned
+    pin: ".right-col-big",
+    pinSpacing: true,
+    anticipatePin: 1,
+  });
+
+  let quickFactsTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".img-fs",
+      start: "top top",
+      scrub: 1,
+      markers: true,
+    },
+  });
+  // quickFactsTl.set(".h1-col, .left-col, .panel-distance-food", {
+  //   pin: true,
+  // })
+  quickFactsTl.from(".fact-1", {
+    yPercent: 100,
+  });
 
   // gsap.set(".img-door", {
 
   // });
 });
 
+gsap.globalTimeline.timeScale(3);
